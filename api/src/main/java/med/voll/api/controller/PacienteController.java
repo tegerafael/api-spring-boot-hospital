5b260b391f2002496bb7d442696eabc65ccf9a28
Tiago Rafael Oliveira das Virgens <tiago.rafael@compasso.com.br>
2023-12-13 09:56:03 -0400

    feat: add paciente crud

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,11 +2,13 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.paciente.DadosCadastroPaciente;
-import med.voll.api.paciente.DadosListagemPaciente;
-import med.voll.api.paciente.Paciente;
-import med.voll.api.paciente.PacienteRepository;
+import med.voll.api.medico.DadosAtualizacaoMedico;
+import med.voll.api.medico.DadosListagemMedico;
+import med.voll.api.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,7 +27,21 @@ public class PacienteController {
     }
 
     @GetMapping
-    public List<DadosListagemPaciente> listar() {
-        return pacienteRepository.findAll().stream().map(DadosListagemPaciente::new).toList();
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return pacienteRepository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
+        var paciente = pacienteRepository.getReferenceById(dados.id());
+        paciente.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        var paciente = pacienteRepository.getReferenceById(id);
+        paciente.excluir();
     }
 }
--- api/src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
+++ api/src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
@@ -2,9 +2,9 @@ package med.voll.api.paciente;
 
 import med.voll.api.endereco.Endereco;
 
-public record DadosListagemPaciente(String nome, String email, String telefone, String cpf, Endereco endereco) {
+public record DadosListagemPaciente(Long id, String nome, String email, String telefone, String cpf, Endereco endereco) {
 
     public DadosListagemPaciente(Paciente paciente) {
-        this(paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco());
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco());
     }
 }
