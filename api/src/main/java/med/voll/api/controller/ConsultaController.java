9109a599202627823ebd489c8a7b2b94b6143f6b
Tiago Rafael Oliveira das Virgens <tiago.rafael@compasso.com.br>
2023-12-19 08:10:15 -0400

    feat: implement business rules

---
+++ api/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -0,0 +1,27 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsultas;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consultas")
+public class ConsultaController {
+
+    @Autowired
+    private AgendaDeConsultas agendaDeConsultas;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+        var dto = agendaDeConsultas.agendar(dados);
+        return ResponseEntity.ok(dto);
+    }
+}
